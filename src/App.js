9511a85918b091ebe2f40d3a2f78f004cd428077
Parrot <parrot@localhost.localdomain>
2022-10-02 11:37:03 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from "./components/Home"
+import Instructions from "./components/Instructions";
+import Quizpage from "./components/Quizpage";
+import Result from "./components/Result";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+   <Routes>
+       <Route path="/" element={<Home/>} />
+       <Route path="/play/instruction" exact element={<Instructions/>} />
+       <Route path="/play/quiz" exact element={<Quizpage/>} />
+       <Route path="/play/result" exact element={<Result/>} />
+    </Routes>
+   </Router>
   );
 }
 
